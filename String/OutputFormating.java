fa65f068557668bab21e3a92c15ebd0fdb82542f
Shubham Jitiya <jitiya66@gmail.com>
2021-04-10 21:17:56 +0530

    Add files via upload
    Output Formatting

---
+++ String/OutputFormating.java
@@ -0,0 +1,23 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class OutputFormating {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+//            System.out.print(s1);
+//            System.out.printf("%15d\n", x);
+
+            System.out.printf("%-15s%03d%n", s1, x);
+
+        }
+        System.out.println("================================");
+
+    }
+}
