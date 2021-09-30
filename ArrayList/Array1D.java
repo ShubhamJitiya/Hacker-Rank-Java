405eab16f1e8f700acbd1b6cc8b92a170840a01a
shyamsar30 <87701039+shyamsar30@users.noreply.github.com>
2021-10-01 00:10:52 +0530

    Scan was closed early.

--- ArrayList/Array1D.java
+++ ArrayList/Array1D.java
@@ -8,18 +8,16 @@ public class Array1D {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        scan.close();
-
         int[] a = new int[n];
 
-        for (int j = 0; j<n;j++)
+        for (int j = 0; j < n; j++)
         {
             a[j] = scan.nextInt();
         }
-
+        scan.close();
         // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        for (int i : a) {
+            System.out.println(i);
         }
     }
-    }
+}
